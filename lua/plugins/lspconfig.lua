d6fbe92b38175d8c458fcd773a6ebb41ecb76d25
Harry Digos <harrydigos@gmail.com>
2025-02-22 18:12:27 +0200

    add typescript tools

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,26 +7,17 @@ return {
         enabled = false,
         exclude = {}, -- filetypes for which you don't want to enable inlay hints
       },
-      -- servers = {
-      --   eslint = {
-      --     settings = {
-      --
-      --       -- Silent the stylistic rules in you IDE, but still auto fix them
-      --       rulesCustomizations = {
-      --         { rule = "style/*", severity = "off", fixable = true },
-      --         { rule = "format/*", severity = "off", fixable = true },
-      --         { rule = "*-indent", severity = "off", fixable = true },
-      --         { rule = "*-spacing", severity = "off", fixable = true },
-      --         { rule = "*-spaces", severity = "off", fixable = true },
-      --         { rule = "*-order", severity = "off", fixable = true },
-      --         { rule = "*-dangle", severity = "off", fixable = true },
-      --         { rule = "*-newline", severity = "off", fixable = true },
-      --         { rule = "*quotes", severity = "off", fixable = true },
-      --         { rule = "*semi", severity = "off", fixable = true },
-      --       },
-      --     },
-      --   },
-      -- },
+      servers = {
+        tsserver = {
+          enabled = false,
+        },
+        ts_ls = {
+          enabled = false,
+        },
+        vtsls = {
+          enabled = false,
+        },
+      },
     },
   },
 }
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shellcheck",
+      "shfmt",
+      "flake8",
+      "typescript-language-server",
+      "eslint-lsp",
+      "lua-language-server",
+      "prettier",
+      "tailwindcss-language-server",
+    },
+  },
+}
